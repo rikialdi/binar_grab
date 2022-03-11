261b81976093606b1019fb9329043e5eeea94162
riki aldi pari <rikialdipari@gmail.com>
2022-03-11 21:37:28 +0700

    file upload

--- src/main/java/com/binar/grab/GrabApplication.java
+++ src/main/java/com/binar/grab/GrabApplication.java
@@ -1,9 +1,14 @@
 package com.binar.grab;
 
+import com.binar.grab.controller.fileupload.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+		FileStorageProperties.class
+})
 public class GrabApplication {
 
 	public static void main(String[] args) {
